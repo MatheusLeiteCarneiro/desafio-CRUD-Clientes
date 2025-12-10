bf903456d455d831f06a68b17cb9199d937ccb27
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-10 17:51:47 -0300

    feat: implemented Find By Id methods

--- src/main/java/com/devmlc/clientCRUD/controllers/ClientController.java
+++ src/main/java/com/devmlc/clientCRUD/controllers/ClientController.java
@@ -1,9 +1,22 @@
 package com.devmlc.clientCRUD.controllers;
 
+import com.devmlc.clientCRUD.dto.ClientDTO;
+import com.devmlc.clientCRUD.services.ClientService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping(value = "/clients")
 public class ClientController {
+    @Autowired
+    private ClientService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<ClientDTO> findById(@PathVariable Long id){
+        return ResponseEntity.ok(service.findById(id));
+    }
 }
--- src/main/java/com/devmlc/clientCRUD/services/ClientService.java
+++ src/main/java/com/devmlc/clientCRUD/services/ClientService.java
@@ -1,13 +1,27 @@
 package com.devmlc.clientCRUD.services;
 
+import com.devmlc.clientCRUD.dto.ClientDTO;
+import com.devmlc.clientCRUD.entities.Client;
+import com.devmlc.clientCRUD.exceptions.ResourceNotFoundException;
 import com.devmlc.clientCRUD.repositories.ClientRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class ClientService {
     @Autowired
     private ClientRepository repository;
 
+    public ClientDTO findById(Long id){
+        Client client  = repository.findById(id).orElseThrow( () -> new ResourceNotFoundException("Resource not Found"));
+        return tranformEntityToDTO(client);
+    }
+
+
+    public ClientDTO tranformEntityToDTO(Client client){
+        return new ClientDTO(client);
+    }
 
 }
