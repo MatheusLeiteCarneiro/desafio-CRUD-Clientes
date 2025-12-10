54d94338c183e0f8e31e758511edc2e7da03eaee
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-10 18:55:33 -0300

    feat: initial data import

---
+++ src/main/resources/import.sql
@@ -0,0 +1,16 @@
+
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Maria Silva', '12345678901', 6500.00, '1985-03-12', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('João Santos', '98765432100', 2800.50, '1992-07-25', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ana Oliveira', '45612378955', 12000.00, '1978-01-15', 3);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Pedro Souza', '32165498711', 4200.00, '1995-11-02', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Lucas Lima', '15975345688', 3500.00, '1989-05-20', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Julia Pereira', '75315985233', 9500.00, '1982-09-10', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Carlos Ferreira', '85296374122', 1800.00, '2001-04-30', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Fernanda Alves', '95135745699', 5400.00, '1990-12-05', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Rafael Costa', '25845612377', 7100.00, '1987-02-18', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Bruno Rodrigues', '65478912300', 3100.00, '1998-08-14', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Amanda Gomes', '14725836911', 2500.00, '1994-06-22', 1);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Ricardo Martins', '36985214722', 15000.00, '1975-10-08', 4);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Gabriel Rocha', '74185296333', 4800.00, '1991-03-03', 2);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Beatriz Barbosa', '15935748655', 2200.00, '2003-01-29', 0);
+INSERT INTO tb_client (name, cpf, income, birth_date, children) VALUES ('Felipe Ribeiro', '96325874144', 6200.00, '1984-11-19', 3);
