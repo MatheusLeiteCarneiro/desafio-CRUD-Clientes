38dcab3b71a269019e44ffec438a3bc63b9f8c2c
Matheus <matheus.leite.carneiro@gmail.com>
2025-12-10 17:39:31 -0300

    feat: create ClientController class

---
+++ src/main/java/com/devmlc/clientCRUD/controllers/ClientController.java
@@ -0,0 +1,9 @@
+package com.devmlc.clientCRUD.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(value = "/clients")
+public class ClientController {
+}
